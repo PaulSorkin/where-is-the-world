348585256ece4947d2b8d8afef6949516e933768
ÑPaul Sorkin <sorkinpavel@mail.ru>
2022-11-22 22:03:09 +0200

    feat: main page is ready

---
+++ src/pages/Details.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {useParams} from "react-router-dom";
+
+const Details = () => {
+    let {name} = useParams()
+
+    return (
+        <div>
+            Details {name}
+        </div>
+    );
+};
+
+export default Details;
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            This page doesn' exist
+        </div>
+    );
+};
+
+export default NotFound;
